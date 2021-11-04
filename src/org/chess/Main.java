67e398e36920d48124c36efc34249b36f0a62933
ShardFenix <psyclone87@juno.com>
2021-11-03 19:31:56 -0500

    Better test output

--- src/org/chess/Main.java
+++ src/org/chess/Main.java
@@ -11,47 +11,65 @@ public class Main {
 		board.print2();
 		check = board.isPlayerInCheck('B');
 		checkmate = board.isPlayerInCheckmate('B');
-		assertThat(!check && !checkmate);
+		assertThat(!check);
+		System.out.println("Check passed.");
+		assertThat(!checkmate);
+		System.out.println("Checkmate passed.");
 		
 		//Black is in checkmate
 		board = new ChessBoard("CheckTest1");
 		board.print2();
 		check = board.isPlayerInCheck('B');
 		checkmate = board.isPlayerInCheckmate('B');
-		assertThat(check && checkmate);
+		assertThat(check);
+		System.out.println("Check passed.");
+		assertThat(checkmate);
+		System.out.println("Checkmate passed.");
 
 		//Black is in check
 		board = new ChessBoard("CheckTest2");
 		board.print2();
 		check = board.isPlayerInCheck('B');
 		checkmate = board.isPlayerInCheckmate('B');
-		assertThat(check && !checkmate);
+		assertThat(check);
+		System.out.println("Check passed.");
+		assertThat(!checkmate);
+		System.out.println("Checkmate passed.");
 
 		//Black is in check, black queen can prevent checkmate
 		board = new ChessBoard("CheckTest3");
 		board.print2();
 		check = board.isPlayerInCheck('B');
 		checkmate = board.isPlayerInCheckmate('B');
-		assertThat(check && !checkmate);
+		assertThat(check);
+		System.out.println("Check passed.");
+		assertThat(!checkmate);
+		System.out.println("Checkmate passed.");
 
 		//Black is in check, black king can take queen to prevent checkmate
 		board = new ChessBoard("CheckTest4");
 		board.print2();
 		check = board.isPlayerInCheck('B');
 		checkmate = board.isPlayerInCheckmate('B');
-		assertThat(check && !checkmate);
+		assertThat(check);
+		System.out.println("Check passed.");
+		assertThat(!checkmate);
+		System.out.println("Checkmate passed.");
 
 		//Black is in checkmate
 		board = new ChessBoard("CheckTest5");
 		board.print2();
 		check = board.isPlayerInCheck('B');
 		checkmate = board.isPlayerInCheckmate('B');
-		assertThat(check && checkmate);
+		assertThat(check);
+		System.out.println("Check passed.");
+		assertThat(checkmate);
+		System.out.println("Checkmate passed.");
 	}
 	
 	public static void assertThat(boolean b) throws Exception {
 		if (!b) {
-			//throw new Exception("Assertion failed.");
+			throw new Exception("Assertion failed.");
 		}
 	}
 }
