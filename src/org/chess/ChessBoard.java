485791dc03bc2b841b5dd7eaa7a07488e61d66da
MuqDonalds <muqdonaldsdev@gmail.com>
2022-07-06 19:51:00 -0500

    klewdfkljadkgjkladjfg

--- src/org/chess/ChessBoard.java
+++ src/org/chess/ChessBoard.java
@@ -2,6 +2,8 @@ package org.chess;
 
 import java.awt.Point;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * This is the file you can edit. You will need to populate isPlayerInCheck
@@ -52,11 +54,6 @@ public class ChessBoard extends AbstractChessBoard {
 					{
 						return true;
 					}
-					/*
-					if (canMoveWithCaptureRule(piece, kingPos.x, kingPos.y) == null)
-					{
-						return true;
-					}*/
 				}
 			}
 		}
@@ -87,6 +84,27 @@ public class ChessBoard extends AbstractChessBoard {
         return chessPieceXY;
     }
 	
+	public ChessPiece playerKingPiece(char c) {
+        ChessPiece piece = null;
+        for (int x = 0; x <= 7; x++) 
+        {
+            for (int y = 0; y <= 7; y++) 
+            {
+                piece = get(x,y);
+                if (piece == null) {
+                    continue;
+                }
+                if (piece.getColor() != c) {
+                    continue;
+                }
+                if (piece.isEssential()){
+                    return piece;
+                }
+            }
+        }
+        return piece;
+	}
+	
 	/**
 	 * For a player 'W' or 'B', return whether that player has an essential piece
 	 * that's in checkmate.
@@ -96,10 +114,140 @@ public class ChessBoard extends AbstractChessBoard {
 	 */
 	@Override
 	public boolean isPlayerInCheckmate(char c) {
-		if (isPlayerInCheck(c))
+		if (!isPlayerInCheck(c))
+		{
+			return false;
+		}
+		Point kingPos = playerKingPos(c);
+		ChessPiece kingPiece = playerKingPiece(c);
+		List<Point> enemyPieces = getEnemyPieces(c);
+		List<Point> alliedPieces = getAlliedPieces(c);
+		List<Point> kingMovableSpaces = getKingMovableSpaces(c);
+		List<Point> kingRuleset = getKingRuleset(c);
+
+		if (kingPos != null && enemyPieces != null) {
+			for (Point enemyPiece : enemyPieces)
+			{
+				if (canMove(kingPiece, enemyPiece.x, enemyPiece.y) == null)
+				{
+					return false;
+				}
+				for (Point alliedPiece : alliedPieces)
+				{
+					 for(Point rulesetSpace : kingRuleset)
+					 {
+						 if (canMove(get(alliedPiece.x,alliedPiece.y), rulesetSpace.x, rulesetSpace.y) == null) 
+						 {
+							 return false;
+						 }
+					 }
+				}
+			}
+		}
+		if (kingMovableSpaces.isEmpty())
 		{
 			return true;
 		}
+
 		return false;
 	}
+	
+	public List<Point> getEnemyPieces(char c)
+	{
+		List<Point> enemyPieces = new LinkedList<Point>();
+		for	(int x = 0; x<= 7; x++)
+		{
+			for (int y = 0; y <= 7; y++)
+			{
+				ChessPiece piece = get(x,y);
+				Point chessPiecePos = new Point();
+				chessPiecePos.x = x;
+				chessPiecePos.y = y;
+				if (piece == null)
+				{
+					continue;
+				}
+				if (piece.getColor() != c)
+				{
+					enemyPieces.add(chessPiecePos);
+				}
+			}
+		}
+		return enemyPieces;
+	}
+	
+	public List<Point> getAlliedPieces(char c)
+	{
+		List<Point> alliedPieces = new LinkedList<Point>();
+		ChessPiece kingPiece = playerKingPiece(c);
+		for	(int x = 0; x<= 7; x++)
+		{
+			for (int y = 0; y <= 7; y++)
+			{
+				ChessPiece piece = get(x,y);
+				Point chessPiecePos = new Point();
+				chessPiecePos.x = x;
+				chessPiecePos.y = y;
+				if (piece == null)
+				{
+					continue;
+				}
+				if (piece.getColor() == c && piece != kingPiece)
+				{
+					alliedPieces.add(chessPiecePos);
+				}
+			}
+		}
+		return alliedPieces;
+	}
+
+	public List<Point> getKingMovableSpaces(char c)
+	{
+		Point kingPos = playerKingPos(c);
+		ChessPiece piece = playerKingPiece(c);
+		List<Point> movableSpaces = new LinkedList<Point>();
+		if (kingPos != null && piece != null)
+		{
+			Point boardSpace = new Point();
+			for	(int x = 0; x<= 7; x++)
+			{
+				for (int y = 0; y <= 7; y++)
+				{
+					boardSpace.x = x;
+					boardSpace.y = y;
+					if (canMove(piece, x, y) == null)
+					{
+						movableSpaces.add(boardSpace);
+					}
+				}
+			}
+		}
+		return movableSpaces;
+	}
+	
+	public List<Point> getKingRuleset(char c)
+	{
+		Point kingPos = playerKingPos(c);
+		ChessPiece piece = playerKingPiece(c);
+		List<Point> ruleset = new LinkedList<Point>();
+		if (kingPos != null && piece != null)
+		{
+			for (int i = kingPos.x - 1; i < kingPos.x + 1; ++i) {
+			    for (int j = kingPos.y - 1; j < kingPos.y + 1; ++j) {
+			        Point square = new Point();
+			        if (kingPos.x != i && kingPos.y != j)
+			        {
+			        	if (i > -1 && j < 9)
+			        	{
+					        square.x = i;
+					        square.y = j;
+					        ruleset.add(square);
+			        	}
+			        }
+			    }
+			}
+		}
+		return ruleset;
+	}
+	
 }
